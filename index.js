2c8a59b757c6e7651755cb1b1c0b0bc1f6f00654
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-08-13 10:38:31 -0600

    Create cli class with initial method names, moved sql files to db directory

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 import inquirer from 'inquirer';
+
+// Create a Cli Class
+
+class CLI {
+    // Create an addEmployee method
+    addEmployee() {
+
+    }
+
+    // Create an updateEmployeeRole method
+    updateEmployeeRole() {
+
+    }
+
+    // Create a viewAllRoles method
+    viewAllRoles() {
+
+    }
+
+    // Create an addRole method
+    addRole() {
+
+    }
+
+    // Create a viewAllDepartments method
+    viewAllDepartments() {
+
+    }
+
+    // Create an addDepartment method
+    addDepartment() {
+
+    }
+}
