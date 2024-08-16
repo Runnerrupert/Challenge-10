82aad7000775012766c86c89110cbb4d3c13cd4c
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-08-16 14:53:16 -0600

    Create an addRole method, update the delete department method to tell the user which department it delets, add comments explaining most of the code in index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,28 @@
 import inquirer from 'inquirer';
 import { pool, connectToDb } from './connection.js';
 import chalk from 'chalk';
+// Waits to initialize the program until the database has been connected from connection.js
 await connectToDb();
+// Initialization function - Prompts the user for what to do in the program
 async function init() {
+    // Variable to hold the choice the user has made
     const answers = await inquirer.prompt({
         name: 'actions',
         type: 'list',
         message: 'What would you like to do?',
         choices: [
             'View All Employees',
-            'Add Employee',
-            'Update Employee Role',
             'View All Roles',
-            'Add Role',
             'View All Departments',
+            'Add Employee',
+            'Add Role',
             'Add Department',
+            'Update Employee Role',
             'Delete Department',
             'Quit'
         ]
     });
+    // Uses the answers variable to run a function based on what was chosen
     switch (answers.actions) {
         case 'View All Employees':
             viewAllEmployees();
@@ -48,34 +52,27 @@ async function init() {
             process.exit(0);
     }
 }
-// Create a viewAllEmployees method
+// Create a viewAllEmployees method - This Method logs a table of all employee specific data
 const viewAllEmployees = async () => {
     const sql = `SELECT * FROM employees`;
+    // A variable for holding the objects retrieved by the sql code
     const res = await pool.query(sql);
-    console.log(chalk.greenBright('Employees:'));
+    // Uses the "Chalk" package to create different colored text within the Command-Line Interface
+    console.log(chalk.whiteBright('Employees:'));
+    // Creates a table using the objects retrieved within the res variable
     console.table(res.rows);
+    // Re-initializes the init function so the user can make another choice
     init();
 };
-// Create an addEmployee method
-const addEmployee = async () => {
-};
-// Create an updateEmployeeRole method
-const updateEmployeeRole = async () => {
-    init();
-};
-// Create a viewAllRoles method
+// Create a viewAllRoles method - This Method logs a table of all roles specific data - Refer to viewAllEmployees
 const viewAllRoles = async () => {
     const sql = `SELECT * FROM roles`;
     const res = await pool.query(sql);
-    console.log(chalk.greenBright('Roles:'));
+    console.log(chalk.whiteBright('Roles:'));
     console.table(res.rows);
     init();
 };
-// Create an addRole method
-const addRole = async () => {
-    init();
-};
-// Create a viewAllDepartments method
+// Create a viewAllDepartments method - This Method logs a table of all department specific data
 const viewAllDepartments = async () => {
     const sql = `SELECT * FROM departments`;
     const res = await pool.query(sql);
@@ -83,28 +80,59 @@ const viewAllDepartments = async () => {
     console.table(res.rows);
     init();
 };
-// Create an addDepartment method
+// Create an addEmployee method - This Method adds an employee to the employees table
+const addEmployee = async () => {
+};
+// Create an addRole method - This Method adds a Role to the roles table
+const addRole = async () => {
+    // Destructures and stores the objects returned by pool.query in a variable
+    const { rows } = await pool.query(`SELECT id, name FROM departments`);
+    // Stores an array of objects built using the .map() method - Each object has a "value" and a "name"
+    const departmentList = rows.map((department) => ({ value: department.id, name: department.name }));
+    // An array that holds information in objects for each question
+    const questions = [
+        { name: 'roleName', type: 'input', message: 'What is the name of the role?' },
+        { name: 'roleSalary', type: 'input', message: 'What is the salary of the role?' },
+        { name: 'departmentName', type: 'list', message: 'Which department does the role belong to?', choices: departmentList }
+    ];
+    // A variable to hold all responses (answers) from the inquirer prompts
+    const answers = await inquirer.prompt(questions);
+    // Adds the role that was created by the user from answering the prompt questions
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)`;
+    // Initializes the sql code with the data given by the user
+    await pool.query(sql, [answers.roleName, answers.roleSalary, answers.departmentName]);
+    // Statement to tell the user that their role has been added
+    console.log(`Added ${answers.roleName} to the database`);
+    // Retrieves the new information from the departments table and logs it to the console formatted as a table
+    const newTable = await pool.query(`SELECT * FROM roles`);
+    console.table(newTable.rows);
+    // Re-initializes the init function so the user can make another choice
+    init();
+};
+// Create an addDepartment method - This Method adds a department to the departments table - Refer to addRole
 async function addDepartment() {
     const questions = [{ name: 'departmentName', type: 'input', message: 'What is the name of the department?' }];
     const answer = await inquirer.prompt(questions);
     const sql = `INSERT INTO departments (name) VALUES ($1)`;
-    const res = await pool.query(sql, [answer.departmentName]);
+    await pool.query(sql, [answer.departmentName]);
     console.log(`Added ${answer.departmentName} to the database`);
-    console.table(res.rows);
+    const newTable = await pool.query(`SELECT * FROM departments`);
+    console.table(newTable.rows);
     init();
 }
-// Create a deleteDepartment method
+// Create an updateEmployeeRole method - This Method updates an existing employee on the employees table
+const updateEmployeeRole = async () => {
+};
+// Create a deleteDepartment method - This method deletes a department from the departments table
 async function deleteDepartment() {
     const { rows } = await pool.query(`SELECT id, name FROM departments`);
     const departmentList = rows.map((department) => ({ value: department.id, name: department.name }));
     const questions = [{ name: 'departmentName', type: 'list', message: 'Which department would you like to delete?', choices: departmentList }];
     const answer = await inquirer.prompt(questions);
-    console.log(rows);
-    console.log(departmentList);
-    console.log(answer);
+    const deletedDepartment = departmentList.find(department => department.value === answer.departmentName)?.name;
     const sql = `DELETE FROM departments WHERE id = $1`;
     await pool.query(sql, [answer.departmentName]);
-    console.log(`Deleted from the database`);
+    console.log(`Deleted ${deletedDepartment} from the database`);
     const newTable = await pool.query(`SELECT * FROM departments`);
     console.table(newTable.rows);
     init();
