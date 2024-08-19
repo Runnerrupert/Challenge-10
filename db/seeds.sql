5b2ea722beb7aea994d6365fdfdbe2688ed5a2ed
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-08-19 10:20:37 -0600

    Delete decimals from inserted roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,14 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id) 
 VALUES 
-    ('Sales Lead', 100000.00, 4),
-    ('Salesperson', 80000.00, 4),
-    ('Lead Engineer', 150000.00, 1),
-    ('Software Engineer', 120000.00, 1),
-    ('Account Manager', 160000.00, 2),
-    ('Accountant', 125000.00, 2),
-    ('Legal Team Lead', 250000.00, 3),
-    ('Lawyer', 190000.00, 3);
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4),
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Account Manager', 160000, 2),
+    ('Accountant', 125000, 2),
+    ('Legal Team Lead', 250000, 3),
+    ('Lawyer', 190000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id) 
 VALUES 
